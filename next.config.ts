3df6ca31de2c6d09eb535c9b72f1f7c66707b476
omi-kanta <anchan0031@icloud.com>
2026-05-26 23:28:09 +0900

    feat: microCMSの画像ドメインを追加

--- next.config.ts
+++ next.config.ts
@@ -1,7 +1,12 @@
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
-  /* config options here */
+  images: {
+    domains: [
+      "images.microcms-assets.io",
+      "images.unsplash.com",
+    ],
+  },
 };
 
 export default nextConfig;
