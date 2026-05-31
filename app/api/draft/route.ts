7b9aba7e355b240f3f580e3f01e4f5e5e2202d95
omi-kanta <anchan0031@icloud.com>
2026-05-31 13:22:35 +0900

    feat: プレビュー機能を追加

---
+++ app/api/draft/route.ts
@@ -0,0 +1,17 @@
+import { draftMode } from 'next/headers'
+import { redirect } from 'next/navigation'
+
+export async function GET(request: Request) {
+  const { searchParams } = new URL(request.url)
+  const secret = searchParams.get('secret')
+  const slug = searchParams.get('slug')
+
+  if (secret !== process.env.MICROCMS_PREVIEW_SECRET) {
+    return new Response('Invalid secret', { status: 401 })
+  }
+
+  const d = await draftMode()
+  d.enable()
+
+  redirect(slug || '/top')
+}
