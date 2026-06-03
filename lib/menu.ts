90480321fffb07b4722461a9d79e9f32261fbe7a
omi-kanta <anchan0031@icloud.com>
2026-06-03 23:36:23 +0900

    feat: microCMSのメニューランチ追加

--- lib/menu.ts
+++ lib/menu.ts
@@ -2,7 +2,7 @@ import { client } from './microcms';
 import { MenuItem } from '@/types/menu';
 
 // メニュー一覧取得
-export const getMenuList = async (category?: 'drink' | 'food') => {
+export const getMenuList = async (category?: 'drink' | 'food' | 'lunch') => {
   try {
     const data = await client.getList<MenuItem>({
       endpoint: 'menu',
@@ -22,6 +22,30 @@ export const getMenuList = async (category?: 'drink' | 'food') => {
 export const getMenuItem = async (id: string, draftKey?: string) => {
   const data = await client.get<MenuItem>({
     endpoint: 'menu',
+    contentId: id,
+    queries: draftKey ? { draftKey } : undefined,
+  });
+  return data;
+};
+
+// ランチ一覧取得
+export const getLunchList = async () => {
+  try {
+    const data = await client.getList<MenuItem>({
+      endpoint: 'lunch',
+      queries: { limit: 100 },
+    });
+    return data.contents;
+  } catch (error) {
+    console.error('microCMS fetch error:', error);
+    return [];
+  }
+};
+
+// ランチ詳細取得
+export const getLunchItem = async (id: string, draftKey?: string) => {
+  const data = await client.get<MenuItem>({
+    endpoint: 'lunch',
     contentId: id,
     queries: draftKey ? { draftKey } : undefined,
   });
--- types/menu.ts
+++ types/menu.ts
@@ -1,17 +1,42 @@
-export type Allergen = '卵' | '乳' | '豚肉' | '小麦' | '大豆' | 'えび' | 'かに';
+export type Allergen =
+  | '卵'
+  | '乳'
+  | '豚肉'
+  | '鶏肉'
+  | '牛肉'
+  | '大豆'
+  | '小麦'
+  | 'ごま'
+  | 'マカダミアナッツ'
+  | 'カシューナッツ'
+  | 'アーモンド'
+  | '落花生（ピーナッツ）'
+  | 'くるみ'
+  | 'そば'
+  | 'かに'
+  | 'えび'
+  | 'さば'
+  | 'さけ'
+  | 'いか'
+  | 'いくら'
+  | 'あわび'
+  | 'オレンジ'
+  | 'りんご'
+  | 'バナナ'
+  | 'キウイ'
+  | 'ゼラチン'
+  | 'もも';
 
 export type MenuItem = {
   id: string;
   name: string;
   subheading: string;
-  category: 'drink'[] | 'food'[];
+  category: 'drink'[] | 'food'[] | 'lunch'[];
   image: { url: string; width: number; height: number };
   description: string;
   ingredients: string;
-  calories: number;
-  protein: number;
-  fat: number;
-  carbs: number;
-  salt: number;
+  price: number;
+  label?: string[];
+  focusNutrient?: string;
   allergens: Allergen[];
 };
